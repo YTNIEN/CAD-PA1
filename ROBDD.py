ecfa2ca39b7bb09c515ba91c901feab6471ce6c2
Nien <nien9304.ee01@nctu.edu.tw>
2017-10-07 20:26:17 +0800

    Initial commit

---
+++ ROBDD.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+'''2017CAD PA1 - Reduction of Ordered Binary Decision Diagram
+'''
+
+import sys
+
+def main():
+    '''Main funciton.
+    '''
+    print('CAD Lab1 - ROBDD')
+
+if __name__ == '__main__':
+    main()
+    
